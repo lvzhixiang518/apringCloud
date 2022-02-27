5d2c5491866d8dccc5fa4746a04431e212bd57a3
lzx19 <962745314@qq.com>
2022-02-27 15:01:39 +0800

    springcloud:远程调用

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -20,7 +20,8 @@ public class OrderService {
         Order order = orderMapper.findById(orderId);
         // 2.利用RestTemplate发起http请求，查询用户
         // 2.1.url路径
-        String url = "http://userservice/user/" + order.getUserId();
+        //String url = "http://userservice/user/" + order.getUserId();
+        String url = "http://localhost:8081/user/" + order.getUserId();
         // 2.2.发送http请求，实现远程调用
         User user = restTemplate.getForObject(url, User.class);
         // 3.封装user到Order
